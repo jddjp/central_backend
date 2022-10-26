dc6a33e1ec7ddaf3b06688563b36efff204b604a
JonaJRSG <jonathan.serna@hotmail.com>
2022-10-26 00:24:53 -0500

    Upgrade & Solve endPoints

--- config/admin.js
+++ config/admin.js
@@ -2,4 +2,7 @@ module.exports = ({ env }) => ({
   auth: {
     secret: env('ADMIN_JWT_SECRET', '8b33ed1907439fd508cc8f776fa556aa'),
   },
+  apiToken: {
+    salt: env('API_TOKEN_SALT'),
+  },
 });

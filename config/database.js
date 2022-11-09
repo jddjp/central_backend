3ab8c106563da3c02dc07902226cf2fb2aa01e0c
Jose Daniel <dejesusperezjosedaniel@gmail.com>
2022-11-09 16:27:28 -0600

    fix

--- config/database.js
+++ config/database.js
@@ -2,12 +2,11 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'mysql',
     connection: {
-      host: env('DATABASE_HOST', '50.31.176.181'),
+      host: env('DATABASE_HOST', '35.202.172.100'),
       port: env.int('DATABASE_PORT', 3306),
-      database: env('DATABASE_NAME', 'ecsxyfhy_db-central'),
-      user: env('DATABASE_USERNAME', 'ecsxyfhy_centraladmin'),
-      password: env('DATABASE_PASSWORD', 'x=4XHVU3BLsW'),
-      // ,x=4XHVU3BLsW
+      database: env('DATABASE_NAME', 'db-central'),
+      user: env('DATABASE_USERNAME', 'centraladmin'),
+      password: env('DATABASE_PASSWORD', '336699@Db'),
       ssl: env.bool('DATABASE_SSL', false),
     },
   },

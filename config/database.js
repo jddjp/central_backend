49e6fbfb3ad0ba66129e40e2c717a07214dcfda2
mariorocha2308 <virgoroch852@gmail.com>
2022-11-08 22:19:58 -0600

    add deploy config database

--- config/database.js
+++ config/database.js
@@ -2,11 +2,11 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'mysql',
     connection: {
-      host: env('DATABASE_HOST', '127.0.0.1'),
+      host: env('DATABASE_HOST', '35.202.172.100'),
       port: env.int('DATABASE_PORT', 3306),
-      database: env('DATABASE_NAME', 'central_backend'),
-      user: env('DATABASE_USERNAME', 'root'),
-      password: env('DATABASE_PASSWORD', ''),
+      database: env('DATABASE_NAME', 'db-central'),
+      user: env('DATABASE_USERNAME', 'centraladmin'),
+      password: env('DATABASE_PASSWORD', '336699@Db'),
       ssl: env.bool('DATABASE_SSL', false),
     },
   },

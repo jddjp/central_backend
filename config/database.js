c5cccb22c39b8e7f004280d4d4e1af711b1a0b0b
Jose Daniel <dejesusperezjosedaniel@gmail.com>
2022-11-16 17:09:51 -0600

    fix:connection bd

--- config/database.js
+++ config/database.js
@@ -4,7 +4,7 @@ module.exports = ({ env }) => ({
     connection: {
       host: env('DATABASE_HOST', '50.31.176.181'),
       port: env.int('DATABASE_PORT', 3306),
-      database: env('DATABASE_NAME', 'ecsxyfhy_db-central'),
+      database: env('DATABASE_NAME', 'ecsxyfhy_ecsxyfhy_db-central'),
       user: env('DATABASE_USERNAME', 'ecsxyfhy_centraladmin'),
       password: env('DATABASE_PASSWORD', 'x=4XHVU3BLsW'),
       // ,x=4XHVU3BLsW

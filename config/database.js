fd67337ee72669afde2cfed0d228a99dcf58d582
Jose Daniel <dejesusperezjosedaniel@gmail.com>
2022-11-16 16:34:43 -0600

    added base CPANEL

--- config/database.js
+++ config/database.js
@@ -2,11 +2,12 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'mysql',
     connection: {
-      host: env('DATABASE_HOST', '35.202.172.100'),
+      host: env('DATABASE_HOST', '50.31.176.181'),
       port: env.int('DATABASE_PORT', 3306),
-      database: env('DATABASE_NAME', 'db-central'),
-      user: env('DATABASE_USERNAME', 'centraladmin'),
-      password: env('DATABASE_PASSWORD', '336699@Db'),
+      database: env('DATABASE_NAME', 'ecsxyfhy_db-central'),
+      user: env('DATABASE_USERNAME', 'ecsxyfhy_centraladmin'),
+      password: env('DATABASE_PASSWORD', 'x=4XHVU3BLsW'),
+      // ,x=4XHVU3BLsW
       ssl: env.bool('DATABASE_SSL', false),
     },
   },

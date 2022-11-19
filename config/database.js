0e7b04294e491e7ca4f02649911922c4a790c383
Jose Daniel <dejesusperezjosedaniel@gmail.com>
2022-11-19 16:29:04 -0600

    update:conexion bd pre produccion

--- config/database.js
+++ config/database.js
@@ -2,12 +2,11 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'mysql',
     connection: {
-      host: env('DATABASE_HOST', '50.31.176.181'),
+      host: env('DATABASE_HOST', '75.102.22.121'),
       port: env.int('DATABASE_PORT', 3306),
-      database: env('DATABASE_NAME', 'ecsxyfhy_ecsxyfhy_db-central'),
-      user: env('DATABASE_USERNAME', 'ecsxyfhy_centraladmin'),
+      database: env('DATABASE_NAME', 'zythqgnc_ComercializadoraSanJoseProd'),
+      user: env('DATABASE_USERNAME', 'zythqgnc_centraladmin'),
       password: env('DATABASE_PASSWORD', 'x=4XHVU3BLsW'),
-      // ,x=4XHVU3BLsW
       ssl: env.bool('DATABASE_SSL', false),
     },
   },

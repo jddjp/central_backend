dee93b114acbe6985d216e8b36297efbb2661a7c
JonaJRSG <jonathan.serna@hotmail.com>
2022-12-13 13:37:21 -0600

    Conexión BD

--- config/database.js
+++ config/database.js
@@ -2,12 +2,12 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'mysql',
     connection: {
-      host: env('DATABASE_HOST', '75.102.22.121'),
+      host: env('DATABASE_HOST', '50.31.176.181'),
       port: env.int('DATABASE_PORT', 3306),
-      database: env('DATABASE_NAME', 'zythqgnc_ComercializadoraSanJoseProd'),
-      user: env('DATABASE_USERNAME', 'zythqgnc_centraladmin'),
+      database: env('DATABASE_NAME', 'ecsxyfhy_ecsxyfhy_db-central'),
+      user: env('DATABASE_USERNAME', 'ecsxyfhy_centraladmin'),
       password: env('DATABASE_PASSWORD', 'x=4XHVU3BLsW'),
-      ssl: env.bool('DATABASE_SSL', false),
-    },
+      ssl: env.bool('DATABASE_SSL', false),
+    },
   },
 });

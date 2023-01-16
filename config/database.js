953fe7316b64b6cf9bee6c1d0f7cb501fda49f7e
JonaJRSG <jonathan.serna@hotmail.com>
2023-01-16 15:58:46 -0600

    Booleano Estatus Pedido

--- config/database.js
+++ config/database.js
@@ -7,7 +7,7 @@ module.exports = ({ env }) => ({
       database: env('DATABASE_NAME', 'ecsxyfhy_ecsxyfhy_db-central'),
       user: env('DATABASE_USERNAME', 'ecsxyfhy_centraladmin'),
       password: env('DATABASE_PASSWORD', 'x=4XHVU3BLsW'),
-      ssl: env.bool('DATABASE_SSL', false),
-    },
+      ssl: env.bool('DATABASE_SSL', false),
+    },
   },
 });

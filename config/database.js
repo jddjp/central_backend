fc95c192656fb5c3729dbf5b9d2b35735741e782
Jose Daniel <dejesusperezjosedaniel@gmail.com>
2023-02-10 10:19:45 -0600

    update database AWS

--- config/database.js
+++ config/database.js
@@ -2,11 +2,11 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'mysql',
     connection: {
-      host: env('DATABASE_HOST', '50.31.176.181'),
+      host: env('DATABASE_HOST', 'strapi-databasec.c1gio5ccujns.us-east-1.rds.amazonaws.com'),
       port: env.int('DATABASE_PORT', 3306),
-      database: env('DATABASE_NAME', 'ecsxyfhy_ecsxyfhy_db-central'),
-      user: env('DATABASE_USERNAME', 'ecsxyfhy_centraladmin'),
-      password: env('DATABASE_PASSWORD', 'x=4XHVU3BLsW'),
+      database: env('DATABASE_NAME', 'comercializadora'),
+      user: env('DATABASE_USERNAME', 'admin'),
+      password: env('DATABASE_PASSWORD', 'AdminUxenRoot'),
       ssl: env.bool('DATABASE_SSL', false),
     },
   },

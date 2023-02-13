cc7e2139abcb4650fd952151fe91f18118de7746
Jose Daniel <dejesusperezjosedaniel@gmail.com>
2023-02-13 10:51:31 -0600

    fix update ecosisten config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,19 +1,19 @@
 module.exports = {
-    apps : [
-      {
-        name: 'central_backend',
-        cwd: '/var/www/central_backend',
-        script: 'npm',
-        args: 'start',
-        env: {
-          NODE_ENV: 'production',
-          PORT: 1380,
-          DATABASE_HOST: '50.31.176.181', // database endpoint
-          DATABASE_PORT: 3306,
-          DATABASE_NAME: 'ecsxyfhy_ecsxyfhy_db-central', // DB name
-          DATABASE_USERNAME: 'ecsxyfhy_centraladmin', // your username for psql
-          DATABASE_PASSWORD: 'x=4XHVU3BLsW', // your password for psql
-        },
+  apps : [
+    {
+      name: 'central_backend',
+      cwd: '/home/ubuntu/central_backend',
+      script: 'npm',
+      args: 'start',
+      env: {
+        NODE_ENV: 'production',
+        PORT: 1380,
+        DATABASE_HOST: 'strapi-databasec.c1gio5ccujns.us-east-1.rds.amazonaws.com', // database endpoint
+        DATABASE_PORT: 3306,
+        DATABASE_NAME: 'comercializadora', // DB name
+        DATABASE_USERNAME: 'admin', // your username for psql
+        DATABASE_PASSWORD: 'AdminUxenRoot', // your password for psql
       },
-    ]
-  };
+    },
+  ]
+};

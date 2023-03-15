8a22d1f8a0583647ba3e78326269f015555da666
nivaldo alejandro quiñones garcia <nivis@nivaldos-MacBook-Pro.local>
2023-03-15 02:11:44 -0600

    add/table-historial_numeros

---
+++ src/api/historial-numero/routes/historial-numero.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * historial-numero router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::historial-numero.historial-numero');
---
+++ src/api/historial-numero/services/historial-numero.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * historial-numero service.
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::historial-numero.historial-numero');

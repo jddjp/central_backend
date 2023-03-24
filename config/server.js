cdcb88a978f4a53b6a8d684ca68bf18946aeeed9
nivaldo alejandro quiñones garcia <nivis@nivaldos-MacBook-Pro.local>
2023-03-24 10:29:35 -0600

    add/CRONJOB - elimincacion automatica despues de una semana de cada registro de la tabla historial_numeros

--- config/server.js
+++ config/server.js
@@ -1,6 +1,29 @@
+
+
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  cron:{
+    enabled: true,
+    tasks: {
+      '* * */23 * * *': async ({strapi}) =>{
+        console.log('Inicia CRON JOB ------------------');
+        const entry = await strapi.entityService.findMany('api::historial-numero.historial-numero');
+        entry.map((element) => {
+          //FECHA DE REGISTRO + 7 DIAS
+          var dateLine = new Date(element.createdAt);
+          dateLine.setDate(dateLine.getDate() + 7)
+          if (new Date() >= dateLine ) {
+            async () => {
+              await strapi.entityService.delete('api::historial-numero.historial-numero', element.id);
+              console.log(element.id, ' -ID Registro Eliminado.....');
+            }
+          }
+        });
+        console.log('Proceso terminado ------------------');
+      }
+    }
+  },
   app: {
     keys: env.array("APP_KEYS"),
   },

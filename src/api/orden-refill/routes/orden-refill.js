d4831534043839a1754ee54ff043c422a52d15f2
David842 <david.djp.developer@gmail.com>
2023-10-31 20:13:20 -0600

    feat: Collection orden refill

---
+++ src/api/orden-refill/routes/orden-refill.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * orden-refill router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::orden-refill.orden-refill');
---
+++ src/api/orden-refill/services/orden-refill.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * orden-refill service.
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::orden-refill.orden-refill');

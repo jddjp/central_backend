83dfbfbd89730fe2ffe802bbe8ba3bfcaa27eeb2
Jose Daniel <dejesusperezjosedaniel@gmail.com>
2023-11-02 11:29:12 -0600

    test yarn esosistem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,14 +3,14 @@ module.exports = {
     {
       name: 'central_backend',
       cwd: '/home/ubuntu/central_backend',
-      script: 'npm',
+      script: 'yarn',
       args: 'start',
       env: {
         NODE_ENV: 'production',
         PORT: 1380,
         DATABASE_HOST: 'strapi-databasec.c1gio5ccujns.us-east-1.rds.amazonaws.com', // database endpoint
         DATABASE_PORT: 3306,
-        DATABASE_NAME: 'comercializadora1', // DB name
+        DATABASE_NAME: 'comercializadora', // DB name
         DATABASE_USERNAME: 'admin', // your username for psql
         DATABASE_PASSWORD: 'AdminUxenRoot', // your password for psql
       },
